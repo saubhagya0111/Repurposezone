4cacce072f83286ee80c51538eac4e72f656fc76
saubhagya0111 <saubhagya0111chopragolu2345@gmail.com>
2024-12-12 22:06:45 +0530

    vercel speedinsights enabled

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Image from "next/image";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -97,6 +97,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <SpeedInsights/>
     </div>
   );
 }
