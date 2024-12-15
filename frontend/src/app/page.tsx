ee0e7e7b07517582e1b059977014a3843e262eb3
saubhagya0111 <chopragolu2345@gmail.com>
2024-12-15 15:49:10 +0530

    fix the frontend changes which was causing deployment failures

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Image from "next/image";
+
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import ContentForm from "../app/components/contentForm";
-import Dashboard from "./dashboard/page";
+// import ContentForm from "../app/components/contentForm";
+// import Dashboard from "./dashboard/page";
 import Loginpage from './auth/login/page'
 export default function Home() {
   return (
